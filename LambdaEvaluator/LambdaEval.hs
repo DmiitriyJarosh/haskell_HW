5a1abff43de59cb75dadd33e497b85c5951f56ad
DmitriiJarosh <dimajarosh@gmail.com>
2018-10-19 21:43:46 +0300

    Constants and ADD added

--- LambdaEvaluator/LambdaEval.hs
+++ LambdaEvaluator/LambdaEval.hs
@@ -2,6 +2,8 @@ module LambdaEval
      where
 type Name = String
 data Term = Var String
+          | Con Int
+          | Add Term Term
           | Lam Name Term
           | App Term Term
         deriving (Show)
@@ -11,6 +13,8 @@ type Subst = [(Name, Term)]
 subst :: Name -> Term -> Term -> Term
 subst n t1 t2 = case t2 of
   Var name -> if name == n then t1 else t2
+  Con num -> t2
+  Add term1 term2 -> Add (subst n t1 term1) (subst n t1 term2)
   Lam name term -> if name /= n then Lam name (subst n t1 term) else t2
   App term1 term2 -> App (subst n t1 term1) (subst n t1 term2)
 
@@ -18,7 +22,18 @@ eval :: Term -> Term
 eval t = case t of
       Var name -> t
       Lam name term1 -> t
+      Con num -> t
+      Add term1 term2 -> case eval term1 of
+        Var name -> Add (Var name) (eval term2)
+        Con num -> case eval term2 of
+          Con num1 -> Con (num + num1)
+          otherwise -> Add (Con num) (eval term2)
+        Lam name term3 -> subst name (eval term2) term3
+        App term3 term4 -> eval (App (eval term3) (eval term4))
+        Add term3 term4 -> eval (Add (eval term3) (eval term4))
       App term1 term2 -> case eval term1 of
-        Var name -> App (Var name) term2
-        Lam name term3 -> subst name term2 term3
-        App term3 term4 -> eval (App (eval term3) term4)
+        Var name -> App (Var name) (eval term2)
+        Con num -> App (Con num) (eval term2)
+        Lam name term3 -> subst name (eval term2) term3
+        App term3 term4 -> eval (App (eval term3) (eval term4))
+        Add term3 term4 -> eval (Add (eval term3) (eval term4))
