c67ae2215619b66f471a60283513bb4a5c8351a2
DmitriiJarosh <dimajarosh@gmail.com>
2018-11-05 16:02:13 +0300

    first version of redux counter

--- LambdaEvaluator/LambdaEval.hs
+++ LambdaEvaluator/LambdaEval.hs
@@ -8,36 +8,31 @@ data Term = Var String
           | App Term Term
         deriving (Show)
 
-type Subst = [(Name, Term)]
-
-subst :: Name -> Term -> Term -> Term
+subst :: Name -> Term -> Term -> (Term, Int)
 subst n t1 t2 = case t2 of
-  Var name -> if name == n then t1 else t2
-  Con num -> t2
-  Add term1 term2 -> Add (subst n t1 term1) (subst n t1 term2)
-  Lam name term -> if name /= n then Lam name (subst n t1 term) else t2
-  App term1 term2 -> App (subst n t1 term1) (subst n t1 term2)
+  Var name -> if name == n then (t1, 1) else (t2, 0)
+  Con num -> (t2, 0)
+  Add term1 term2 -> (Add (fst $ subst n t1 term1) (fst $ subst n t1 term2), max (snd $ subst n t1 term1) (snd $ subst n t1 term2))
+  Lam name term -> if name /= n then (Lam name (fst $ subst n t1 term), snd $ subst n t1 term) else (t2, 0)
+  App term1 term2 -> (App (fst $ subst n t1 term1) (fst $ subst n t1 term2), max (snd $ subst n t1 term1) (snd $ subst n t1 term2))
 
-eval :: Term -> Term
-eval t = case t of
-      Var name -> t
-      Lam name term1 -> t
-      Con num -> t
-      Add term1 term2 -> case eval term1 of
-        Var name -> Add (Var name) (eval term2)
-        Con num -> case eval term2 of
-          Con num1 -> Con (num + num1)
-          otherwise -> Add (Con num) (eval term2)
-        Lam name term3 -> case subst name (eval term2) term3 of
-	  Add (Con num1) (Con num2) -> Con (num1 + num2)
-	  otherwise -> subst name (eval term2) term3
-        App term3 term4 -> eval (App (eval term3) (eval term4))
-        Add term3 term4 -> eval (Add (eval term3) (eval term4))
-      App term1 term2 -> case eval term1 of
-        Var name -> App (Var name) (eval term2)
-        Con num -> App (Con num) (eval term2)
-        Lam name term3 -> case subst name (eval term2) term3 of
-	  Add (Con num1) (Con num2) -> Con (num1 + num2)
-	  otherwise -> subst name (eval term2) term3
-        App term3 term4 -> eval (App (eval term3) (eval term4))
-        Add term3 term4 -> eval (Add (eval term3) (eval term4))
+
+eval :: Term -> Int -> (Term, Int)
+eval t cnt = case t of
+      Var name -> (t, cnt)
+      Lam name term1 -> (t, cnt)
+      Con num -> (t, cnt)
+      Add term1 term2 -> case fst $ eval term1 0 of
+        Var name -> (Add (Var name) (fst $ eval term2 0), (snd $ eval term2 0) + cnt)
+        Con num -> case fst $ eval term2 0 of
+          Con num1 -> (Con (num + num1), (snd $ eval term2 0) + cnt + 1)
+          otherwise -> (Add (Con num) (fst $ eval term2 0), (snd $ eval term2 0) + cnt)
+        Lam name term3 -> (t, cnt)
+        App term3 term4 -> (fst $ eval (App (fst $ eval term3 0) (fst $ eval term4 0)) 0, snd $ eval (App (fst $ eval term3 0) (fst $ eval term4 0)) ((snd $ eval term3 0) + (snd $ eval term4 0)))
+        Add term3 term4 -> (fst $ eval (Add (fst $ eval term3 0) (fst $ eval term4 0)) 0, snd $ eval (Add (fst $ eval term3 0) (fst $ eval term4 0)) ((snd $ eval term3 0) + (snd $ eval term4 0)))
+      App term1 term2 -> case fst $ eval term1 0 of
+        Var name -> (App (Var name) (fst $ eval term2 0), cnt + (snd $ eval term2 0))
+        Con num -> (App (Con num) (fst $ eval term2 0), cnt + (snd $ eval term2 0))
+        Lam name term3 -> (fst $ eval (fst $ subst name (fst $ eval term2 0) term3) 0, cnt + (snd $ eval (fst $ subst name (fst $ eval term2 0) term3) ((snd $ eval term2 0) + (snd $ subst name (fst $ eval term2 0) term3))))
+        App term3 term4 -> (fst $ eval (App (fst $ eval term3 0) (fst $ eval term4 0)) 0, snd $ eval (App (fst $ eval term3 0) (fst $ eval term4 0)) ((snd $ eval term3 0) + (snd $ eval term4 0)))
+        Add term3 term4 -> (fst $ eval (Add (fst $ eval term3 0) (fst $ eval term4 0)) 0, snd $ eval (Add (fst $ eval term3 0) (fst $ eval term4 0)) ((snd $ eval term3 0) + (snd $ eval term4 0)))
